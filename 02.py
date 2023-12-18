899847277580e9da80f0c108e368a96c434130ec
Iran Filho <Iran_filho@yahoo.com.br>
2023-12-18 19:16:28 -0300

    adicionando else restante

--- 02.py
+++ 02.py
@@ -10,7 +10,11 @@ def calculadora(numeroUm, operacao, numeroDois):
 
     elif operacao == "/":
       return numeroUm / numeroDois
-
+    
+    else:
+        print("Operação não reconhecida ou divisão por zero. Resultado: 0")
+        return 0
+    
 numeroUm = float(input("Digite o primeiro número: "))
 operacao = input("Digite a operação (+, -, *, /): ")
 numeroDois = float(input("Digite o segundo número: "))
