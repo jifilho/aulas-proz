29fe03040a4104d326f666cee8c360dffdc10654
Iran Filho <Iran_filho@yahoo.com.br>
2023-12-18 19:45:15 -0300

    resolvendo exercicio 03

---
+++ 03.py
@@ -0,0 +1,6 @@
+nomeUsuario = input("Escreva seu nome completo: ")
+anoNascimento = int(input("Digite o ano do seu nascimento: "))
+
+resultadoNascimento = 2022 - anoNascimento
+
+print("Seja bem vindo", nomeUsuario, "você possui", resultadoNascimento, "anos.")
